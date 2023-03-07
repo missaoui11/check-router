2459ae0d5abaef8bd67dcfbd72809558627ee4b5
missaoui11 <ahmedmiss519@gmail.com>
2023-03-07 16:41:31 +0100

    first

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>welcome to contact page</h1>
+      </div>
+    )
+  }
+}
